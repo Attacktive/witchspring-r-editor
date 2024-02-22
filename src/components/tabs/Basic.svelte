051cb9fccf05010a440ddafa76241f1dfeb06d32
Attacktive <tribute2pantera@gmail.com>
2024-02-23 00:53:18 +0900

    feature: improve layout

--- src/components/tabs/Basic.svelte
+++ src/components/tabs/Basic.svelte
@@ -19,79 +19,79 @@
 		}
 	});
 </script>
-<div class="my-1">
+<div class="my-2">
 	<Label>Player Object ID</Label>
 	<Input bind:value={$store.playerObjID}/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Player Object ID (original)</Label>
 	<Input bind:value={$store.playerObjID_Original}/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Dress ID</Label>
 	<Input bind:value={$store.dressID}/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Max HP</Label>
 	<NumberInput bind:value={$store.maxHp} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>HP</Label>
 	<NumberInput bind:value={$store.hp} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Max MP</Label>
 	<NumberInput bind:value={$store.maxMp} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>MP</Label>
 	<NumberInput bind:value={$store.mp} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>SP</Label>
 	<NumberInput bind:value={$store.sp} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Fame</Label>
 	<NumberInput bind:value={$store.fame} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Battle EXP</Label>
 	<NumberInput bind:value={$store.battleEXP} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Body Level</Label>
 	<NumberInput bind:value={$store.bodyLevel} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Mental Level</Label>
 	<NumberInput bind:value={$store.mindLevel} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Physical Training Count</Label>
 	<NumberInput bind:value={$store.bodyTrainingCount} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Mental Training Count</Label>
 	<NumberInput bind:value={$store.mindTrainingCount} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Weapon Charging Speed Factor</Label>
 	<NumberInput bind:value={$store.weaponChargingSpeed} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Walking Speed Factor</Label>
 	<NumberInput bind:value={$store.walkSpeed} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Gold</Label>
 	<NumberInput bind:value={$store.gold} step="1"/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Has Black Joe</Label>
 	<Checkbox bind:checked={$store.blackJoe}/>
 </div>
-<div class="my-1">
+<div class="my-2">
 	<Label>Current Pet</Label>
 	<Select bind:value={$store.nowPet} items={petOptions}/>
 </div>
