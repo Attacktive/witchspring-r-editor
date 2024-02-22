23019d815af1226dcf1db079f76ab5367260589c
Attacktive <tribute2pantera@gmail.com>
2024-02-23 00:53:18 +0900

    feature: improve layout

--- src/components/tabs/Items.svelte
+++ src/components/tabs/Items.svelte
@@ -5,12 +5,12 @@
 </script>
 {#each $store.myItemList as item, index (`item-${index}`)}
 	{@const ordinal = index + 1}
-	<div class="ms-1">#{ordinal} ({getItemCategory(item.category)})</div>
+	<div class="my-1">#{ordinal} ({getItemCategory(item.category)})</div>
 	<div class="my-1">
 		<Label>Item ID</Label>
 		<Input bind:value={item.id}/>
 	</div>
-	<div class="my-1">
+	<div class="my-1 mb-3">
 		<Label>Item Count</Label>
 		<NumberInput bind:value={item.count} step="1"/>
 	</div>
