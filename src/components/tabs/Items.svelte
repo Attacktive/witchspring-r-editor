6a18f49de16c6ed586b7bad4ab920b939a812ba1
Attacktive <tribute2pantera@gmail.com>
2024-02-25 19:14:16 +0900

    feature: implement editing Stats augment

--- src/components/tabs/Items.svelte
+++ src/components/tabs/Items.svelte
@@ -1,21 +1,25 @@
 <script lang="ts">
-	import { Input, Label, NumberInput } from "flowbite-svelte";
+	import { Accordion, AccordionItem, Input, Label, NumberInput } from "flowbite-svelte";
 	import { saveData } from "$store/save-data";
 	import { getItemCategory } from "$types/item";
 </script>
 {#if $saveData.myItemList.length > 0}
-	{#each $saveData.myItemList as item, index (`item-${index}`)}
-		{@const ordinal = index + 1}
-		<div class="my-1">#{ordinal} ({getItemCategory(item.category)})</div>
-		<div class="my-1">
-			<Label>Item ID</Label>
-			<Input bind:value={item.id}/>
-		</div>
-		<div class="my-1 mb-3">
-			<Label>Item Count</Label>
-			<NumberInput bind:value={item.count} step="1"/>
-		</div>
-	{/each}
+	<Accordion>
+		{#each $saveData.myItemList as item, index (`item-${index}`)}
+			{@const ordinal = index + 1}
+			<AccordionItem>
+				<span slot="header">#{ordinal} {item.id} ({getItemCategory(item.category)})</span>
+				<div class="my-1">
+					<Label>Item ID</Label>
+					<Input bind:value={item.id}/>
+				</div>
+				<div class="mt-1 mb-3">
+					<Label>Item Count</Label>
+					<NumberInput bind:value={item.count} step="1"/>
+				</div>
+			</AccordionItem>
+		{/each}
+	</Accordion>
 {:else}
 	No content
 {/if}
