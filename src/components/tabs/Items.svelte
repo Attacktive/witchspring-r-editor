fb1bfe1da9ac6e19cedd28d1e1a3fb45c2616099
Attacktive <tribute2pantera@gmail.com>
2024-02-27 12:43:36 +0900

    feature: implement item addition/removal
    https://github.com/Attacktive/witchspring-r-editor/issues/18

--- src/components/tabs/Items.svelte
+++ src/components/tabs/Items.svelte
@@ -1,9 +1,72 @@
 <script lang="ts">
-	import { Accordion, AccordionItem, Input, Label, NumberInput } from "flowbite-svelte";
+	import type { Item } from "$types/item";
+	import { Accordion, AccordionItem, Button, Input, Label, Modal, NumberInput, Select, SpeedDial, SpeedDialButton } from "flowbite-svelte";
+	import { CirclePlusSolid, TrashBinOutline } from "flowbite-svelte-icons";
 	import { saveData } from "$store/save-data";
-	import { getItemCategory } from "$types/item";
+	import { getItemCategory, itemCategories, nonExhaustItemIdList } from "$types/item";
+
+	const categoryOptions = itemCategories.map(itemCategory => ({
+		name: itemCategory.text,
+		value: itemCategory.index
+	}));
+
+	console.debug(JSON.stringify($saveData.myItemList.map(x => x.id).sort()));
+
+	let toShowModal = false;
+	const showItemCreationModal = () => toShowModal = true;
+
+	const generateNewItem = (): Item => ({
+		id: "",
+		category: 0,
+		count: 1
+	});
+
+	let newItem = generateNewItem();
+
+	const createItem = () => {
+		$saveData.myItemList.push(newItem);
+		newItem = generateNewItem();
+	};
+
+	const throwAway = (index: number) => {
+		$saveData.myItemList = $saveData.myItemList.filter((_, innerIndex) => index !== innerIndex);
+	};
 </script>
+
+<datalist id="item-id-list">
+	{#each nonExhaustItemIdList as itemId}
+		<option>{itemId}</option>
+	{/each}
+</datalist>
+
+<Modal bind:open={toShowModal}>
+	<form class="flex flex-col">
+		<h2 class="text-xl">New Item</h2>
+		<div class="my-2">
+			<Label>ID</Label>
+			<Input bind:value={newItem.id} list="item-id-list"/>
+		</div>
+		<div class="my-2">
+			<Label>Category</Label>
+			<Select bind:value={newItem.category} items={categoryOptions}/>
+		</div>
+		<div class="my-2">
+			<Label>Count</Label>
+			<Input bind:value={newItem.count}/>
+		</div>
+		<Button class="my-2" on:click={createItem}>
+			<CirclePlusSolid/>
+		</Button>
+	</form>
+</Modal>
+
 {#if $saveData.myItemList.length > 0}
+	<SpeedDial defaultClass="absolute top-32 end-12">
+		<SpeedDialButton on:click={showItemCreationModal}>
+			<CirclePlusSolid class="w-8 h-8"/>
+		</SpeedDialButton>
+	</SpeedDial>
+
 	<Accordion>
 		{#each $saveData.myItemList as item, index (`item-${index}`)}
 			{@const ordinal = index + 1}
@@ -11,11 +74,16 @@
 				<span slot="header">#{ordinal} {item.id} ({getItemCategory(item.category)})</span>
 				<div class="my-1">
 					<Label>Item ID</Label>
-					<Input bind:value={item.id}/>
+					<Input bind:value={item.id} list="item-id-list"/>
 				</div>
 				<div class="mt-1 mb-3">
 					<Label>Item Count</Label>
 					<NumberInput bind:value={item.count} step="1"/>
+				</div>
+				<div class="flex flex-row-reverse">
+					<Button on:click={() => throwAway(index)}>
+						<TrashBinOutline/>
+					</Button>
 				</div>
 			</AccordionItem>
 		{/each}
--- src/types/stick-action.ts
+++ src/types/stick-action.ts
@@ -1,3 +1,3 @@
-export const stickActionIds: StickAction[] = ["StickAttack", "StickAttack1", "StickAttack2", "StickAttack7", "StickAttack8", "StickAttack9", "StickAttack10", "StickAttack12", "StickAttackMini"] as const;
+export const stickActionIds: readonly StickAction[] = Object.freeze(["StickAttack", "StickAttack1", "StickAttack2", "StickAttack7", "StickAttack8", "StickAttack9", "StickAttack10", "StickAttack12", "StickAttackMini"]);
 
 export type StickAction = "StickAttack" | "StickAttack1" | "StickAttack2" | "StickAttack7" | "StickAttack8" | "StickAttack9" | "StickAttack10" | "StickAttack12" | "StickAttackMini";
