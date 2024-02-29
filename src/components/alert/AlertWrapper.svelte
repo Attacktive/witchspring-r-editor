763f3fac305379592c868f58b949d32b99dcd24e
Attacktive <tribute2pantera@gmail.com>
2024-02-29 11:09:45 +0900

    refactor: clean up code around Alerts
    https://github.com/Attacktive/witchspring-r-editor/issues/30

---
+++ src/components/alert/AlertWrapper.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import { Button } from "flowbite-svelte";
+
+	export let messageLines: string[] = [];
+	export let acknowledgedText = "Got it";
+
+	const dispatch = createEventDispatcher();
+	const closeAlert = () => dispatch("close");
+</script>
+
+{#each messageLines as messageLine}
+	<p>{messageLine}</p>
+{/each}
+<Button size="sm" class="mt-2" on:click={closeAlert}>
+	{acknowledgedText}
+</Button>
