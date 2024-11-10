83c39c2a5667e00d3bbe40b3d348f9f311b4aaaa
Attacktive <tribute2pantera@gmail.com>
2024-11-10 16:09:26 +0900

    feature: add filters to Items
    https://github.com/Attacktive/witchspring-r-editor/issues/13

--- src/components/tabs/Items.svelte
+++ src/components/tabs/Items.svelte
@@ -1,14 +1,36 @@
 <script lang="ts">
-	import type { Item } from "$/types/item";
-	import { Accordion, AccordionItem, Button, Input, Label, Modal, NumberInput, Select, SpeedDial, SpeedDialButton } from "flowbite-svelte";
-	import { CirclePlusSolid, TrashBinOutline } from "flowbite-svelte-icons";
+	import type { Filter, Item, ItemCategoryIndex, NullableItemCategoryIndex } from "$/types/item";
+	import { Accordion, AccordionItem, Button, Input, Label, Modal, NumberInput, Select, type SelectOptionType, SpeedDial, SpeedDialButton } from "flowbite-svelte";
+	import { CirclePlusSolid, SearchOutline, TrashBinOutline } from "flowbite-svelte-icons";
 	import { saveData } from "$/store/save-data";
 	import { getItemCategory, itemCategories, nonExhaustiveItemIdList } from "$/types/item";
 
-	const categoryOptions = itemCategories.map(itemCategory => ({
+	const categoryOptions: SelectOptionType<ItemCategoryIndex>[] = itemCategories.map(itemCategory => ({
 		name: itemCategory.text,
 		value: itemCategory.index
 	}));
+
+	const categoryOptionsWithNone: SelectOptionType<NullableItemCategoryIndex>[] = [
+		{ name: "Any categories", value: -1 },
+		...categoryOptions
+	];
+
+	const filter = $state<Filter>({ id: "", category: -1 });
+	const passesFiltration = (item: Item) => {
+		if (filter.id.length > 0) {
+			if (!item.id.toLowerCase().includes(filter.id.toLowerCase())) {
+				return false;
+			}
+		}
+
+		if (filter.category >= 0) {
+			if (filter.category !== item.category) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 
 	console.debug(JSON.stringify($saveData.myItemList.map(x => x.id).sort()));
 
@@ -67,27 +89,36 @@
 		</SpeedDialButton>
 	</SpeedDial>
 
-	<Accordion>
+	<div class="flex justify-between space-x-20 mb-3">
+		<Input placeholder="ID" bind:value={filter.id} class="">
+			<SearchOutline slot="left"/>
+		</Input>
+		<Select placeholder="Category" bind:value={filter.category} items={categoryOptionsWithNone} class=""/>
+	</div>
+
+	<Accordion multiple>
 		{#each $saveData.myItemList as item, index (`item-${index}`)}
 			{@const ordinal = index + 1}
-			<AccordionItem>
-				{#snippet header()}
-					<span >#{ordinal} {item.id} ({getItemCategory(item.category)})</span>
-				{/snippet}
-				<div class="my-1">
-					<Label>Item ID</Label>
-					<Input bind:value={item.id} list="item-id-list"/>
-				</div>
-				<div class="mt-1 mb-3">
-					<Label>Item Count</Label>
-					<NumberInput bind:value={item.count} step="1"/>
-				</div>
-				<div class="flex flex-row-reverse">
-					<Button on:click={() => throwAway(index)}>
-						<TrashBinOutline/>
-					</Button>
-				</div>
-			</AccordionItem>
+			{#if passesFiltration(item)}
+				<AccordionItem>
+					{#snippet header()}
+						<span >#{ordinal} {item.id} ({getItemCategory(item.category)})</span>
+					{/snippet}
+					<div class="my-1">
+						<Label>Item ID</Label>
+						<Input bind:value={item.id} list="item-id-list"/>
+					</div>
+					<div class="mt-1 mb-3">
+						<Label>Item Count</Label>
+						<NumberInput bind:value={item.count} step="1"/>
+					</div>
+					<div class="flex flex-row-reverse">
+						<Button on:click={() => throwAway(index)}>
+							<TrashBinOutline/>
+						</Button>
+					</div>
+				</AccordionItem>
+			{/if}
 		{/each}
 	</Accordion>
 {:else}
