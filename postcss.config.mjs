a81ae295780bc9e83ab21eeaf44d2dc856eef0cb
Attacktive <tribute2pantera@gmail.com>
2024-12-22 15:44:36 +0900

    chore: resolve warning 'Method expression is not of Function type'

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -4,7 +4,7 @@ import autoprefixer from "autoprefixer";
 const config = {
 	plugins: [
 		// Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(),
+		tailwindcss,
 		// But others, like autoprefixer, need to run after,
 		autoprefixer
 	]
